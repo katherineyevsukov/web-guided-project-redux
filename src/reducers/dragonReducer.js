370d135b23f5688341cd30ec3c7c6782458c6d71
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-05 13:33:20 -0400

    lecture work-in-progress

---
+++ src/reducers/dragonReducer.js
@@ -0,0 +1,5 @@
+//1. initialState related to our dragonMember UI.
+//      - members: array of member object { name: 'Jojo Zhang', dragonStatus: true }
+
+//2. reducer for dragonMember UI
+//      - addMember action
