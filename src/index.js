125147d2af312f9bf89d468a5d9f231a9cf84e60
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-05 12:25:51 -0400

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
-import {Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+
+
+
 
 function App() {
   return (
