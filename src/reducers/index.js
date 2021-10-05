48b5d4f676af251f7a0a223fbb733dfe73abcb37
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-05 14:11:36 -0400

    lecture work-in-progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,6 @@ import { combineReducers } from 'redux';
 import dragonReducer from './dragonReducer';
 import titleReducer from './titleReducer';
 
-console.log(dragonReducer);
-console.log(titleReducer);
-
 const rootReducer = combineReducers({
   dragonReducer,
   titleReducer
