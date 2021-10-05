a8b91cc48b5955e6d949a0964944d570f954bc3c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-05 14:04:07 -0400

    lecture work-in-progress

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -14,8 +14,7 @@ class DragonList extends React.Component {
   };
 
   handleClick = ()=> {
-    
-    this.dispatch(addMember(this.state.newMember));
+    this.props.dispatch(addMember(this.state.newMember));
     // this.setState({
     //   ...this.state,
     //   members: [...this.members, {name: newMember, dragonStatus: true}]
