8995621bcba6ed147ed4753a01896351b8381103
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-05 13:53:51 -0400

    lecture work-in-progress

--- src/reducers/dragonReducer.js
+++ src/reducers/dragonReducer.js
@@ -1,12 +1,21 @@
-export const initialState = {
-    
-}
-
 //1. initialState related to our dragonMember UI.
 //      - members: array of member object { name: 'Jojo Zhang', dragonStatus: true }
+
+export const initialState = {
+    members: [
+        { name: 'Jojo Zhang', dragonStatus: true },
+        { name: 'Brandon Harris', dragonStatus: false }
+    ]
+}
+
 
 //2. reducer for dragonMember UI
 //      - ADD_MEMBER case: return members with a new member added
 //      - default case: return state
 
+const reducer = (state = initialState, action)=> {
+
+}
+
 //3. export out our reducer
+export default reducer;
